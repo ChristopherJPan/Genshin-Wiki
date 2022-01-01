07391a33ab330e54ea1f6ca43afa79999d2cada8
Relai1 <christopherjpan@gmail.com>
2021-12-31 23:51:53 -0800

    deleted typescript

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => {
-//   res.status(200).sendFile(path.join(__dirname, "../src/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../src/index.html"));
+});
 
 // app.use()
 app.get("/*", (req, res) => {
--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 
 
 
-const App = (props) => {
-  return (
-    <div>
-      <p>helloo!!!!!!!!!!!!!!</p>
-    </div>
-  )
-}
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-
-//     return (
-//       <div>
-//         HELLOO!!!
-//       </div>
-//     );
-//   }
+// const App = (props) => {
+//   return (
+//     <div>
+//       <p>helloo!!!!!!!!!!!!!!</p>
+//     </div>
+//   )
 // }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>
+        HELLOO!!!
+      </div>
+    );
+  }
+}
 
 export default App;
